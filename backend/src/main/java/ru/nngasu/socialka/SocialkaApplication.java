1cbf2af36c89a5683f6e9b7b65985ff62edb681f
Gregr <darishur3@gmail.com>
2026-02-11 18:21:56 +0300

    Version2(изменена структура backend)

---
+++ backend/src/main/java/ru/nngasu/socialka/SocialkaApplication.java
@@ -0,0 +1,11 @@
+package ru.nngasu.socialka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialkaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SocialkaApplication.class, args);
+    }
+}
